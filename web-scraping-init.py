e34878f207e45fa2225105a1742649b905071577
MBrugnaroto <brugnaroto@gmail.com>
2022-05-07 18:01:33 -0300

    init wokr

--- web-scraping-init.py
+++ web-scraping-init.py
@@ -1,6 +1,18 @@
-def main(word: str) -> None:
-    print(word)
+from bs4 import BeautifulSoup as Soup
+from urllib.request import urlopen as URLOpen
+
+
+def returnH1Value(url: str) -> None:
+    response = URLOpen(url)
+    html = response.read()
+
+    formatted_html = Soup(html, 'html.parser')
+    result = formatted_html.find('h1', {'class': 'sub-header'}).getText()
+
+    print(f'{result}')
 
 
 if __name__ == '__main__':
-    main(1)
+    url = 'https://alura-site-scraping.herokuapp.com/hello-world.php'
+
+    returnH1Value(url)
