78e8696affb27a8d8cb08d307ce7a33c04fdf800
mbrugnaroto <brugnaroto@gmail.com>
2022-05-14 12:44:48 -0300

    feact: functions created to convert dataframe column type and date format

---
+++ apache-spark/code/utils/constants/main.py
@@ -0,0 +1,23 @@
+path_raw_data =  '../raw-data'
+
+header_companies = ['cnpj_basico', 'razao_social_nome_empresarial', 
+                    'natureza_juridica', 'qualificacao_do_responsavel', 
+                    'capital_social_da_empresa', 'porte_da_empresa', 
+                    'ente_federativo_responsavel']
+
+header_establishments = ['cnpj_basico', 'cnpj_ordem', 'cnpj_dv', 
+                         'identificador_matriz_filial', 'nome_fantasia', 
+                         'situacao_cadastral', 'data_situacao_cadastral', 
+                         'motivo_situacao_cadastral', 'nome_da_cidade_no_exterior', 
+                         'pais', 'data_de_inicio_atividade', 'cnae_fiscal_principal', 
+                         'cnae_fiscal_secundaria', 'tipo_de_logradouro', 
+                         'logradouro', 'numero', 'complemento', 'bairro', 
+                         'cep', 'uf', 'municipio', 'ddd_1', 'telefone_1', 
+                         'ddd_2', 'telefone_2', 'ddd_do_fax', 'fax', 'correio_eletronico', 
+                         'situacao_especial', 'data_da_situacao_especial']
+
+header_partners = ['cnpj_basico', 'identificador_de_socio', 
+                   'nome_do_socio_ou_razao_social', 'cnpj_ou_cpf_do_socio', 
+                   'qualificacao_do_socio', 'data_de_entrada_sociedade', 
+                   'pais', 'representante_legal', 'nome_do_representante', 
+                   'qualificacao_do_representante_legal', 'faixa_etaria'] 
