1838dcff6256921391e16d0612ce1c7ab2be8b7c
mbrugnaroto <brugnaroto@gmail.com>
2022-05-23 22:39:39 -0300

    feat: bash to import dump to database

---
+++ sql-server/up-dump-database.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+dump(){
+	for file in *
+ 	do
+                mysql -u root DB_TEST < $file
+		if [ $? -eq 0 ]
+		then
+			echo "Dump from" $file "was peformed"
+		else
+			echo "Dump from" $file "was not performed"
+		fi
+	done
+}
+
+LOCAL=$(ls | grep dump)
+
+if [ $(pwd | grep dump) ]
+then
+        dump
+elif [ "${LOCAL:-0}" != 0 ]
+then
+        cd dump/
+        dump
+else
+        echo "Dump folder not found."
+fi
+
+
+
