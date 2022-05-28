3dc54b80a3320fe797ce260c9a9fdba2bea9b147
mbrugnaroto <brugnaroto@gmail.com>
2022-05-27 22:58:01 -0300

    feact: environment startup script

---
+++ sql-server/prepare-environment.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+DB='DB_TEST'
+
+/bin/bash create-database.sh $DB
+/bin/bash up-dump-database.sh $DB
+/bin/bash create-generate-random-number-function.sh $DB
+/bin/bash create-generate-random-client-function.sh $DB
+/bin/bash create-generate-random-product-function.sh $DB
+/bin/bash create-generate-random-seller-function.sh $DB
+/bin/bash create-handle-query-function.sh $DB
+/bin/bash create-query-executor-procedure.sh $DB
+/bin/bash create-insert-sale-procedure.sh $DB
